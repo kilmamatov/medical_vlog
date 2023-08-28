5260ce568da38565c1c5594887e46cba4eee4298
Rustam <117253418+kilmamatov@users.noreply.github.com>
2023-08-28 17:13:44 +0500

    Pre main (#10)
    * Feature/refactoring (#6)
    
    * refactoring, optimize import etc
    
    * added update profile fields
    
    * added logout, user's profile delete (#9)
    
    * refactoring, optimize import etc (#5)
    
    * refactoring, optimize import etc (#5) (#1)
    
    * Feature/refactoring (#6) (#7)
    
    * refactoring, optimize import etc
    
    * added update profile fields
    
    Co-authored-by: Avdeevkonst <116953059+Avdeevkonst@users.noreply.github.com>
    
    * added logout, delete userprofile
    
    ---------
    
    Co-authored-by: Rustam <117253418+kilmamatov@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Avdeevkonst <116953059+Avdeevkonst@users.noreply.github.com>

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -4,16 +4,22 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from user_auth.views import RegisterUserView, UserModelView, LoginUserView
+from user_auth.views import (
+    RegisterUserView,
+    UserModelView,
+    LoginUserView,
+    LogoutUserView,
+)
 
 auth = [
     path("create_user/", RegisterUserView.as_view()),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token-refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("profile/", UserModelView.as_view(), name="profile"),
     path("login/", LoginUserView.as_view(), name="login"),
+    path("logout/", LogoutUserView.as_view(), name="logout"),
 ]
 
 urlpatterns = [
     path("auth/", include(auth)),
+    path("profile/<int:pk>", UserModelView.as_view(), name="profile"),
 ]
