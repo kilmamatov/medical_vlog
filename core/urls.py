cff57805bdc8e577221cc535b0c5c64513f36464
Rustam <117253418+kilmamatov@users.noreply.github.com>
2023-08-29 20:42:15 +0500

    create model CommentModel (#13)
    create crud CommentModel
    add slug PostModel

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
 
-from core.views import PostViewSet, TagViewSet
+from core.views import PostViewSet, TagViewSet, CommentViewSet
 
 urlpatterns = []
 
 router = DefaultRouter()
-router.register("tags", TagViewSet, basename="tags")
-router.register("posts", PostViewSet, basename="posts")
+router.register("tags", TagViewSet, basename="tag")
+router.register("posts", PostViewSet, basename="post")
+router.register(r'posts/(?P<slug>[-\w]+)/comments', CommentViewSet, basename='comment')
 urlpatterns += router.urls
